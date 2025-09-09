e8fab5dc662fbde8663111805d19e0660d52215a
ErickWarden <erickwarden@outlook.com>
2025-09-09 16:46:25 -0500

    Added player.py

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     gameClock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     
     # game while loop
@@ -19,6 +21,7 @@ def main():
         
         # main screen presentation
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
